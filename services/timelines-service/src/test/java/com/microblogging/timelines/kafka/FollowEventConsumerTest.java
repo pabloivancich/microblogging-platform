0baf895a5cadfe86b93a3b7f6714d360958e1966
Pablo Ivancich <pabloivancich@gmail.com>
2025-09-09 01:04:04 -0300

    Minor changes in timelines service

--- services/timelines-service/src/test/java/com/microblogging/timelines/kafka/FollowEventConsumerTest.java
+++ services/timelines-service/src/test/java/com/microblogging/timelines/kafka/FollowEventConsumerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @ExtendWith(MockitoExtension.class)
 public class FollowEventConsumerTest {
